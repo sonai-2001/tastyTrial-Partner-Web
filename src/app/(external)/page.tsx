143db4d5c1914de401b92e837e9476c3fc3fd729
Apurba Bhadra <apurba.bhadra@webskitters.in>
2026-01-12 18:50:27 +0530

    check push

--- src/app/(external)/page.tsx
+++ src/app/(external)/page.tsx
@@ -6,7 +6,7 @@ import LandingPage from '@/module/Landing/pages/LandingPage';
 export default function Home() {
   const storageTokenKeyName = process.env.NEXT_PUBLIC_TOKEN_NAME;
   const storedToken = parseCookies(null, storageTokenKeyName as string);
-
+  console.log(process.env.data);
   if (storedToken[storageTokenKeyName as string]) {
     redirect('/authenticated/home');
   } else {
